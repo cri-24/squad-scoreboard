8a51af937b4d37328fc6f14722aa329237b65f21
cri-24 <cristianaangiuoni4@gmail.com>
2024-08-02 23:37:15 +0200

    refactor: modify login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="login-page">
-    <Home />
-    <LoginForm />
+    <div class="form-container">
+      <h2>Accedi</h2>
+      <LoginForm />
+    </div>
   </div>
 </template>
 
 <script setup>
 import LoginForm from '@/components/LoginForm.vue';
-import Home from '@/views/Home.vue';
 </script>
-
-<style scoped>
+<style>
 .login-page {
-  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 100vh;
-  background: url('/path/to/your/background-image.jpg') no-repeat center center/cover;
+  background-color: #f8f9fa;
+}
+
+.form-container {
+  background-color: #ffffff;
+  padding: 2rem;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  text-align: center;
+  width: 100%;
+  max-width: 400px;
+  position: relative;
+}
+
+h2 {
+  color: #007bff; /* Colore vivace per il testo */
+  margin-bottom: 1rem;
 }
 </style>
