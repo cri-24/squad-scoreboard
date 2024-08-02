7e15069c9e84c7ae41b43f5f5c773237eac33ce4
cri-24 <cristianaangiuoni4@gmail.com>
2024-08-02 23:35:04 +0200

    refactor: add import

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import 'bootstrap'; 
-import router from './router/index.js'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import router from './router/index.js';
 
 const app = createApp(App);
 app.use(router);
