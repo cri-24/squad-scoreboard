5c7e7b944f57fb1261c39d3330fb27f5861f3df0
cri-24 <cristianaangiuoni4@gmail.com>
2024-08-02 23:33:41 +0200

    refactor: add app style

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,34 @@
 
 <style>
 body {
-  font-family: Arial, sans-serif;
-  background-color: #f8f9fa;
+  background-color: #e9ecef; /* Colore di sfondo chiaro */
   margin: 0;
   height: 100vh;
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
+  overflow: hidden; /* Evita lo scroll */
+}
+
+#app {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* Sfondo scuro con trasparenza */
+  z-index: 1;
+  pointer-events: none; /* Non cliccabile */
+}
+
+.form-container {
+  position: relative;
+  z-index: 2;
 }
 </style>
