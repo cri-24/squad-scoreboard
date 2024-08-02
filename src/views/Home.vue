91119d35485b46cdd898ddc5c1288d0ef47df5ea
cri-24 <cristianaangiuoni4@gmail.com>
2024-08-02 20:42:35 +0200

    refactor: add home view

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home-page">
+    <TeamList />
+  </div>
+</template>
+
+<script setup>
+import TeamList from '@/components/TeamList.vue';
+</script>
+
+<style scoped>
+.home-page {
+  padding: 2rem;
+}
+</style>
