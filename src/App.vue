2b4ea712ff54b4c0d3e8faa6cff97c1a5a209aa2
cri-24 <cristianaangiuoni4@gmail.com>
2024-08-03 00:36:09 +0200

    refactor: remove style

--- src/App.vue
+++ src/App.vue
@@ -5,36 +5,3 @@
 <script setup>
 </script>
 
-<style>
-body {
-  background-color: #e9ecef; /* Colore di sfondo chiaro */
-  margin: 0;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden; /* Evita lo scroll */
-}
-
-#app {
-  position: relative;
-  width: 100%;
-  height: 100%;
-}
-
-.overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5); /* Sfondo scuro con trasparenza */
-  z-index: 1;
-  pointer-events: none; /* Non cliccabile */
-}
-
-.form-container {
-  position: relative;
-  z-index: 2;
-}
-</style>
