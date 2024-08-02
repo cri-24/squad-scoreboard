89299cd361d4db345c581fae1e4c35df126e64a6
cri-24 <cristianaangiuoni4@gmail.com>
2024-08-02 20:40:59 +0200

    refactor: add basic main with pinia

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';  // Importa il router correttamente
+import { createPinia } from 'pinia';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
+const pinia = createPinia();
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+app.use(router);
+app.use(pinia);
+app.mount('#app');
