019958bc2ebdf60be6b5c49747b58c01f5add233
cri-24 <cristianaangiuoni4@gmail.com>
2024-08-02 20:42:45 +0200

    refactor: create login view

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login-page">
+    <Home />
+    <LoginForm />
+  </div>
+</template>
+
+<script setup>
+import LoginForm from '@/components/LoginForm.vue';
+import Home from '@/views/Home.vue';
+</script>
+
+<style scoped>
+.login-page {
+  position: relative;
+  height: 100vh;
+  background: url('/path/to/your/background-image.jpg') no-repeat center center/cover;
+}
+</style>
