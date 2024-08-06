4eea924baa57558586f2091e1d4c5d7a2104cd6f
cri-24 <cristianaangiuoni4@gmail.com>
2024-08-06 10:22:19 +0200

    refactor: add services

--- src/components/TeamModal.vue
+++ src/components/TeamModal.vue
@@ -1,60 +1,78 @@
 <template>
-    <div class="modal fade show" tabindex="-1" style="display: block;" aria-labelledby="teamModalLabel" aria-hidden="true">
+  <div class="modal fade show" tabindex="-1" style="display: block;" aria-labelledby="teamModalLabel" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="teamModalLabel">Dettagli della squadra</h5>
-            <button type="button" class="btn-close" @click="closeModal" aria-label="Close"></button>
+          <div class="modal-content">
+              <div class="modal-header">
+                  <h5 class="modal-title" id="teamModalLabel">Dettagli della squadra</h5>
+                  <button type="button" class="btn-close" @click="closeModal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                  <h3>{{ team.name }}</h3>
+                  <ul>
+                      <li v-for="member in team.members" :key="member">{{ member }}</li>
+                  </ul>
+                  <h3>Animatori</h3>
+                  <ul>
+                      <li v-for="leader in team.leaders" :key="leader">{{ leader }}</li>
+                  </ul>
+                  <!-- Sezione "Cosa dobbiamo fare domani" -->
+                  <div class="tomorrow-tasks">
+                      <h3>Cosa dobbiamo fare domani</h3>
+                      <img :src="team.serviceimage" alt="Service Image" class="service-image">
+                      <p>{{ team.service }}</p>
+                  </div>
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" @click="closeModal">Chiudi</button>
+              </div>
           </div>
-          <div class="modal-body">
-            <h3>{{ team.name }}</h3>
-            <ul>
-              <li v-for="member in team.members" :key="member">{{ member }}</li>
-            </ul>
-            <h3>{{ 'Animatori' }}</h3>
-            <ul>
-              <li v-for="leader in team.leaders" :key="leader">{{ leader }}</li>
-            </ul>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" @click="closeModal">Chiudi</button>
-          </div>
-        </div>
       </div>
-    </div>
+  </div>
 </template>
-  
+
 <script setup>
 const props = defineProps({
-    team: {
+  team: {
       type: Object,
       required: true
-    }
-  });
-  
-  const emit = defineEmits(['close']);
-  
-  const closeModal = () => {
-    emit('close');
-  };
- </script>
-  
+  }
+});
+
+const emit = defineEmits(['close']);
+
+const closeModal = () => {
+  emit('close');
+};
+</script>
+
 <style scoped>
-  .modal-content {
-    border-radius: 12px; /* Bordi arrotondati */
-    background-color: #ffffff; /* Sfondo della modale bianco */
-  }
-  
-  .modal-body {
-    color: #333; /* Colore del testo */
-  }
-  
-  .modal-body ul {
-    list-style: none;
-    padding: 0;
-  }
-  
-  .modal-body li {
-    font-family: 'Arial', sans-serif;
-  }
+.modal-content {
+  border-radius: 12px; /* Bordi arrotondati */
+  background-color: #ffffff; /* Sfondo della modale bianco */
+}
+
+.modal-body {
+  color: #333; /* Colore del testo */
+}
+
+.modal-body ul {
+  list-style: none;
+  padding: 0;
+}
+
+.modal-body li {
+  font-family: 'Arial', sans-serif;
+}
+
+/* Stile per la sezione "Cosa dobbiamo fare domani" */
+.tomorrow-tasks {
+  margin-top: 20px;
+  text-align: center;
+}
+
+.tomorrow-tasks .service-image {
+  max-width: 30%;
+  height: auto;
+  margin-bottom: 15px;
+}
 </style>
--- src/store/teams-store.js
+++ src/store/teams-store.js
@@ -1,15 +1,17 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import squadImage from '@/assets/squad.jpg';
+import dishwashing from '@/assets/dish-washing.png';
+import setTable from '@/assets/set-table.png';
 
 export const useTeamsStore = defineStore('teams', () => {
   const teams = ref([
-    { name: 'Squadra 1', image: squadImage, members: ['Player1', 'Player2', 'Player3', 'Player4', 'Player5'], leaders: ['Player1', 'Player2', 'Player3'], details: { 'Notturno': 50, 'Giochi acqua': 50, 'Stellone': 50 } },
-    { name: 'Squadra 2', image: squadImage, members: ['Player1', 'Player2', 'Player3', 'Player4', 'Player5'], leaders: ['Player1', 'Player2', 'Player3'], details: { 'Game 1': 40, 'Game 2': 30, 'Game 3': 50 } },
-    { name: 'Squadra 3', image: squadImage, members: ['Player1', 'Player2', 'Player3', 'Player4', 'Player5'], leaders: ['Player1', 'Player2', 'Player3'], details: { 'Game 1': 40, 'Game 2': 30, 'Game 3': 50 }  },
-    { name: 'Squadra 4', image: squadImage, members: ['Player1', 'Player2', 'Player3', 'Player4', 'Player5'], leaders: ['Player1', 'Player2', 'Player3'], details: { 'Game 1': 40, 'Game 2': 30, 'Game 3': 50 }  },
-    { name: 'Squadra 5', image: squadImage, members: ['Player1', 'Player2', 'Player3', 'Player4', 'Player5'], leaders: ['Player1', 'Player2', 'Player3'], details: { 'Game 1': 40, 'Game 2': 30, 'Game 3': 50 } },
-    { name: 'Squadra 6', image: squadImage, members: ['Player1', 'Player2', 'Player3', 'Player4', 'Player5'], leaders: ['Player1', 'Player2', 'Player3'], details: { 'Game 1': 40, 'Game 2': 30, 'Game 3': 50 }  }
+    { name: 'Squadra 1', image: squadImage, serviceimage: setTable, service: "Pranzo", members: ['Player1', 'Player2', 'Player3', 'Player4', 'Player5'], leaders: ['Player1', 'Player2', 'Player3'], details: { 'Notturno': 50, 'Giochi acqua': 50, 'Stellone': 50 } },
+    { name: 'Squadra 2', image: squadImage, serviceimage: dishwashing, service: "Colazione", members: ['Player1', 'Player2', 'Player3', 'Player4', 'Player5'], leaders: ['Player1', 'Player2', 'Player3'], details: { 'Game 1': 40, 'Game 2': 30, 'Game 3': 50 } },
+    { name: 'Squadra 3', image: squadImage, serviceimage: setTable, service: "Cena", members: ['Player1', 'Player2', 'Player3', 'Player4', 'Player5'], leaders: ['Player1', 'Player2', 'Player3'], details: { 'Game 1': 40, 'Game 2': 30, 'Game 3': 50 }  },
+    { name: 'Squadra 4', image: squadImage, serviceimage: dishwashing, service: "Cena", members: ['Player1', 'Player2', 'Player3', 'Player4', 'Player5'], leaders: ['Player1', 'Player2', 'Player3'], details: { 'Game 1': 40, 'Game 2': 30, 'Game 3': 50 }  },
+    { name: 'Squadra 5', image: squadImage, serviceimage: setTable, service: "Pranzo", members: ['Player1', 'Player2', 'Player3', 'Player4', 'Player5'], leaders: ['Player1', 'Player2', 'Player3'], details: { 'Game 1': 40, 'Game 2': 30, 'Game 3': 50 } },
+    { name: 'Squadra 6', image: squadImage, serviceimage: setTable, service: "Colazione", members: ['Player1', 'Player2', 'Player3', 'Player4', 'Player5'], leaders: ['Player1', 'Player2', 'Player3'], details: { 'Game 1': 40, 'Game 2': 30, 'Game 3': 50 }  }
   ]);
 
   return { teams };
